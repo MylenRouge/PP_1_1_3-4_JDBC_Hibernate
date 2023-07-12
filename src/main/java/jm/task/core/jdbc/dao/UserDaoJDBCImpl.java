9cbef8abac808bd64418f3acf73f9e0f8fc4c817
MylenRouge <pasha-sklyarov@live.com>
2023-07-12 16:15:33 +0300

    JDBC connection

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,109 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.net.ConnectException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
+    Connection connection = Util.getConnection();
 
+    public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-
+        String query = "CREATE TABLE IF NOT EXISTS user (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(255), lastname VARCHAR(255), age TINYINT) ";
+        try {
+            connection.setAutoCommit(false);
+            Statement statement = connection.createStatement();
+            statement.execute(query);
+            connection.commit();
+            connection.setAutoCommit(true);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
+        String query = "DROP TABLE IF EXISTS user";
+        try {
+            connection.setAutoCommit(false);
+            Statement statement = connection.createStatement();
+            statement.execute(query);
+            connection.commit();
+            connection.setAutoCommit(true);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        String query = "INSERT INTO user (id, name, lastname, age) VALUES (?, ?, ?, ?)";
+        try {
+            connection.setAutoCommit(false);
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, 0); // Здесь нужно указать значение для id
+            statement.setString(2, name);
+            statement.setString(3, lastName);
+            statement.setByte(4, age);
+            statement.executeUpdate();
+            connection.commit();
+            connection.setAutoCommit(true);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
     public void removeUserById(long id) {
-
+        String query = "DELETE FROM user WHERE id = ?";
+        try {
+            connection.setAutoCommit(false);
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setLong(1, id);
+            statement.executeUpdate();
+            connection.commit();
+            connection.setAutoCommit(true);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        try {
+            Connection connection = Util.getConnection();
+            PreparedStatement statement = connection.prepareStatement("select * from user");
+            ResultSet result = statement.executeQuery();
+            while (result.next()) {
+                User user = new User();
+                user.setId(result.getLong("id"));
+                user.setName(result.getString("name"));
+                user.setLastName(result.getString("lastname"));
+                user.setAge(result.getByte("age"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        String query = "TRUNCATE TABLE user";
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            connection.setAutoCommit(false);
+            statement.execute();
+            connection.commit();
+            connection.setAutoCommit(true);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,28 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
+    private static final String DRIVER = "con.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/jdbctest";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "FuckTheSystem2189";
+
+    public Util() {
+
+    }
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Connected");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
     // реализуйте настройку соеденения с БД
 }
