8f2c3d3756b5363c33352038b1e7ab54f3005b87
Pavel Sklyarov <pasha-sklyarov@live.com>
2023-08-10 17:29:52 +0300

    Update Util.java
    r

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -15,7 +15,7 @@ import java.util.Properties;
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/JDBCtest";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "FuckTheSystem2189";
+    private static final String PASSWORD = "root";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DIALECT = "org.hibernate.dialect.MySQLDialect";
     private static SessionFactory sessionFactory;
